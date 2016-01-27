0b54c9cf6ffec30a084b33ea37777c4dfc6558f6
Amaranth YAN <amaranth.yan@gmail.com>
2016-01-27 01:02:02 +0100

    更改Config文件路径,使用UnityPersistencePath替代DataPath;略微优化Config读取与写入

--- Assets/Scripts/Configuration.cs
+++ Assets/Scripts/Configuration.cs
@@ -4,31 +4,43 @@ using System.Xml;
 using System.IO;
 
 public class Configuration {
-	public static string ConfigFile = "/Xmls/Config.xml";
+	public static string ConfigFile = "/Config.xml";
 
 	public static void LoadConfig(string paramName, out int param) {
 		XmlDocument xmlFile = new XmlDocument();
-		xmlFile.Load(Application.dataPath + ConfigFile);
-		XmlNodeList config = xmlFile.GetElementsByTagName(paramName);
-		if (!int.TryParse(config[0].InnerText, out param)) {
+		try {
+			xmlFile.Load(Application.persistentDataPath + ConfigFile);
+			XmlNodeList config = xmlFile.GetElementsByTagName(paramName);
+			if (!int.TryParse(config[0].InnerText, out param)) {
+				param = 0;
+				Debug.LogError("Parameter ERROR! Do NOT modify the Config file!");
+			}
+		} catch(DirectoryNotFoundException e) {
 			param = 0;
-			Debug.LogError("Parameter ERROR! Do NOT modify the Config file!");
+			Debug.LogError("Unity data directory MISSING!");
+		} catch(FileNotFoundException e) {
+			param = 0;
+			Debug.LogError("Config file MISSING! Use default value.");
 		}
 	}
 	
 	public static void SaveConfig(string paramName, int value) {
 		XmlNode param;
 		XmlDocument xmlFile = new XmlDocument();
-		xmlFile.Load(Application.dataPath + ConfigFile);
+		try {
+			xmlFile.Load(Application.persistentDataPath + ConfigFile);
+		} catch(DirectoryNotFoundException e) {
+			Directory.CreateDirectory(Application.persistentDataPath);
+		} catch(FileNotFoundException e) {
+		}
 		XmlNodeList config = xmlFile.GetElementsByTagName(paramName);
 		if (config.Count > 0) {
 			param = config[0];
 		} else {
 			param = xmlFile.CreateElement(paramName);
-			XmlElement root = xmlFile.DocumentElement;
-			root.AppendChild(param);
+			xmlFile.AppendChild(param);
 		}
 		param.InnerText = value.ToString();
-		xmlFile.Save(Application.dataPath + ConfigFile);
+		xmlFile.Save(Application.persistentDataPath + ConfigFile);
 	}
 }
